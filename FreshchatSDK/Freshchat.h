70232f8726c1cdc788a771b7d39c914263d557e3
Grigory Avdyushin <avdyushin.g@gmail.com>
2021-01-26 15:51:10 +0100

    Freshchat SDK 4.1.4 added

---
+++ FreshchatSDK/Freshchat.h
@@ -0,0 +1,15 @@
+//
+//  FreshchatSDK.h
+//  FreshchatSDK
+//
+//  Created by Grigory Avdyushin on 26/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+#import "FreshchatSDK.h"
+
+//! Project version number for FreshchatSDK.
+FOUNDATION_EXPORT double FreshchatSDKVersionNumber;
+
+//! Project version string for FreshchatSDK.
+FOUNDATION_EXPORT const unsigned char FreshchatSDKVersionString[];
